730c529c91d0445486efbe7e8451537e74785d84
Pelin Mutanguha <pelmut2000@gmail.com>
2018-11-12 14:23:28 +0200

    Add project files.

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public void Display()
+        {
+            Console.WriteLine("Hello From Student Class");
+        }
+    }
+}
